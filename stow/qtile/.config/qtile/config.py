3e5dbd788ede17a4069144aca3c6a628e00f562b
Xyroton <xyroton@users.noreply.github.com>
2023-03-12 19:36:16 +0100

    changed icons qtile

--- stow/qtile/.config/qtile/config.py
+++ stow/qtile/.config/qtile/config.py
@@ -137,8 +137,8 @@ groups = [
         Group("1", label=""),
         Group("2", label=""),
         Group("3", label=""),
-        Group("4", label="󰈄"),
-        Group("5", label=""),
+        Group("4", label=""),
+        Group("5", label="󰈄"),
         Group("6", label="󰉔"),
         Group("7", label=""),
         ]
